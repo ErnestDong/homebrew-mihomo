b2d160c30752241c851096d524dc3b12fad08a7a
Ernest Dong <dongcy2000@gmail.com>
2024-01-03 22:51:17 +0800

    add clashx.meta cask

---
+++ Casks/clashx-meta.rb
@@ -0,0 +1,10 @@
+cask "clashx-meta" do
+  version "1.3.8"
+
+  url "https://github.com/MetaCubeX/ClashX.Meta/releases/download/v#{version}/ClashX.Meta.macOS.12.0+.zip"
+  name "ClashX Meta"
+  desc "Rule based proxy base on mihomo"
+  homepage "https://github.com/MetaCubeX/ClashX.Meta/"
+
+  app "ClashX Meta.app"
+end

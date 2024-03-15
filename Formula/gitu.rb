8be13da2a677eb618aa57e7000402bb60dcae65b
Ernest Dong <dongcy2000@gmail.com>
2024-03-15 18:40:57 +0800

    bug fix

--- Formula/gitu.rb
+++ Formula/gitu.rb
@@ -1,7 +1,8 @@
 class Gitu < Formula
+  version "0.6.3"
   desc "A TUI Git client inspired by Magit"
   homepage "https://github.com/altsem/gitu"
-  url "https://github.com/altsem/gitu/releases/download/v0.6.3/gitu-v0.6.3-x86_64-apple-darwin.tar.gz"
+  url "https://github.com/altsem/gitu/releases/download/v#{version}/gitu-v#{version}-x86_64-apple-darwin.tar.gz"
   head "https://github.com/altsem/gitu.git"
 
   def install
